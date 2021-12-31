0e76a74bfc0633353fabafab43f8d9383a295d3f
Jannis Hell <Primajin@users.noreply.github.com>
2021-12-31 14:47:25 +0100

    :umbrella: add codecov

--- test/apply-key.js
+++ test/apply-key.js
@@ -1,6 +1,10 @@
 import test from 'ava';
 import {applyKey} from '../index.js';
 
-test('abc', t => {
+test('abc on UPPERCASE', t => {
 	t.is(applyKey('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.', [0, 1, 2]), 'TIG QVKCL DRPYN GQX KWMQU OWGR UJE MCZZ FOH.');
+});
+
+test('abc on lowercase', t => {
+	t.is(applyKey('the quick brown fox jumps over the lazy dog.', [0, 1, 2]), 'tig qvkcl drpyn gqx kwmqu owgr uje mczz foh.');
 });
