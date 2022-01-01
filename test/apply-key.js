2bc4f3a6b674c33fe81f56a288a652965e0b6a1f
Jannis Hell <Primajin@users.noreply.github.com>
2022-01-01 16:10:23 +0100

    :dango: add permutations

--- test/apply-key.js
+++ test/apply-key.js
@@ -9,6 +9,12 @@ test('abc on lowercase', t => {
 	t.is(applyKey('the quick brown fox jumps over the lazy dog.', [0, 1, 2]), 'tig qvkcl drpyn gqx kwmqu owgr uje mczz foh.');
 });
 
-test('empty on lowercase', t => {
-	t.is(applyKey('This just stays the same as before!', []), 'This just stays the same as before!');
+test('permutations', t => {
+	t.is(applyKey('AaA', [1]), 'BbB'); // just one
+	t.is(applyKey('AaA', [1, 1]), 'BbB'); // key shorter than message
+	t.is(applyKey('AaA', [1, 1, 1]), 'BbB'); // key equal length to message
+	t.is(applyKey('AaA', [1, 1, 1, 1]), 'BbB'); // key longer than message
+	t.is(applyKey('BbB', [1, 1, 1]), 'CcC'); // key same as message
+	t.is(applyKey('AaA', [0, 0, 0]), 'AaA'); // key only zeroes
+	t.is(applyKey('AaA', []), 'AaA'); // key empty
 });
