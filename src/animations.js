c8fc7ac0b8714488efde404856a166c0e7ccb6b7
Jannis Hell <Primajin@users.noreply.github.com>
2022-01-11 07:49:48 +0100

    :key: apply key

--- src/animations.js
+++ src/animations.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-await-in-loop,no-promise-executor-return */
+import {applyKey} from './cypher.js';
 /**
  * Sleeps for the given amount of time.
  * @param {number} milliseconds
@@ -7,30 +8,24 @@
 const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
 
 /**
- * Does nothing.
- */
-const noop = () => {};
-
-/**
  * Counts down and mutates each item in the array to 0, from beginning to the end.
  * @param {Array} keyArray - The array to count down.
  * @param {number} delay - The delay between each item.
- * @param {Function} [functionToCallWithKeyArray] - The function to call each time with the key array.
  * @param {boolean} [reverse=false] - Whether to count down from the end to the beginning.
  * @returns {Promise<void>}
  */
-export const countdownKeyArray = async (keyArray, delay, functionToCallWithKeyArray = noop(), reverse = false) => {
+export const countdownKeyArray = async (keyArray, delay, reverse = false) => {
 	const countDown = async (value, index) => {
 		for (let i = value - 1; i >= 0; i--) {
 			await sleep(delay);
 			keyArray[index] = i;
-			functionToCallWithKeyArray(keyArray);
+			console.log('applyKey', applyKey('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.', keyArray));
 		}
 	};
 
 	if (reverse) {
-		for (let index = keyArray.length - 1; index >= 0; index--) {
-			await countDown(keyArray[index], index);
+		for (let index = keyArray.length - 1, value = keyArray[index]; index >= 0; index--) {
+			await countDown(value, index);
 		}
 	} else {
 		for (const [index, value] of keyArray.entries()) {
@@ -39,9 +34,9 @@ export const countdownKeyArray = async (keyArray, delay, functionToCallWithKeyAr
 	}
 };
 
-const myArray = [5, 5, 5, 5];
-await countdownKeyArray(myArray, 100, console.log, true);
+const myArray = [5, 5, 5];
+await countdownKeyArray(myArray, 100, true);
 await sleep(1000);
 console.log('');
-const myArray2 = [5, 5, 5, 5];
-await countdownKeyArray(myArray2, 100, console.log);
+const myArray2 = [5, 5, 5];
+await countdownKeyArray(myArray2, 100);
