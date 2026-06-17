ee5f609736461d24711e56788e78be537a9d7b3b
Lint Action <Primajin@users.noreply.github.com>
2026-06-17 17:03:34 +0000

    :sparkles: Fix code style issues with XO

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import {applyKey, makeKeyArray} from './src/cypher.js';
 
 /**
- * Encrypts given text with given key / password
- * @param {string} text
- * @param {string} key
- * @returns {string}
+ Encrypts given text with given key / password
+ @param {string} text
+ @param {string} key
+ @returns {string}
  */
 export const encrypt = (text, key) => applyKey(text, makeKeyArray(key));
 
 /**
- * Decrypts given text with given key / password
- * @param {string} text
- * @param {string} key
- * @returns {string}
+ Decrypts given text with given key / password
+ @param {string} text
+ @param {string} key
+ @returns {string}
  */
 export const decrypt = (text, key) => applyKey(text, makeKeyArray(key).map(item => ((26 - item) % 26)));
 
--- src/cypher.js
+++ src/cypher.js
@@ -1,38 +1,39 @@
 /**
- * Checks if given code point is within Latin uppercase range
- * The code point for 'A' is 65, until the code point for 'Z' is 90.
- * @param {number} codePoint
- * @returns {boolean}
+ Checks if given code point is within Latin uppercase range
+ The code point for 'A' is 65, until the code point for 'Z' is 90.
+ @param {number} codePoint
+ @returns {boolean}
  */
 export const isInLatinUppercaseRange = codePoint => codePoint >= 65 && codePoint <= 90;
 
 /**
- * Checks if given code point is within Latin lowercase range
- * The code point for 'a' is 97, until the code point for 'z' is 122.
- * @param {number} codePoint
- * @returns {boolean}
+ Checks if given code point is within Latin lowercase range
+ The code point for 'a' is 97, until the code point for 'z' is 122.
+ @param {number} codePoint
+ @returns {boolean}
  */
 export const isInLatinLowercaseRange = codePoint => codePoint >= 97 && codePoint <= 122;
 
 /**
- * Checks if given code point is within Latin range
- * The code point for 'A' is 65, until the code point for 'z' is 122.
- * @param {number} codePoint
- * @returns {boolean}
+ Checks if given code point is within Latin range
+ The code point for 'A' is 65, until the code point for 'z' is 122.
+ @param {number} codePoint
+ @returns {boolean}
  */
 export const isWithinLatinRange = codePoint => codePoint >= 65 && codePoint <= 122;
 
 /**
- * Applies the given key onto the text
- * @param text {string}
- * @param key {Array}
- * @returns {string}
+ Applies the given key onto the text
+ @param text {string}
+ @param key {Array}
+ @returns {string}
  */
 export const applyKey = (text, key) => {
-	let cipherText = '';
 	if (!key || key.length === 0) {
 		return text;
 	}
+
+	let cipherText = '';
 
 	let offset = 0;
 	for (const letter of text) {
@@ -52,15 +53,15 @@ export const applyKey = (text, key) => {
 };
 
 /**
- * Returns an array of the code points for the given key between 0 and 26
- * @example
- * // returns [0, 1, 2]
- * makeKeyArray('abc');
- * @example
- * // returns [1, 4, 11, 11, 0, 18, 14]
- * makeKeyArray('Bellaso!!Äö@');
- * @param key
- * @returns {Array}
+ Returns an array of the code points for the given key between 0 and 26
+ @example
+ // returns [0, 1, 2]
+ makeKeyArray('abc');
+ @example
+ // returns [1, 4, 11, 11, 0, 18, 14]
+ makeKeyArray('Bellaso!!Äö@');
+ @param key
+ @returns {Array}
  */
 export const makeKeyArray = key => {
 	const keyArray = [];
