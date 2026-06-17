bb130e5492944b1a21677c7d563d4afee77707a0
Lint Action <Primajin@users.noreply.github.com>
2026-06-17 17:03:34 +0000

    :sparkles: Fix code style issues with XO

--- src/animations.js
+++ src/animations.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-await-in-loop,no-promise-executor-return */
 import {applyKey} from './cypher.js';
 /**
- * Sleeps for the given amount of time.
- * @param {number} milliseconds
- * @returns {Promise<unknown>}
+ Sleeps for the given amount of time.
+ @param {number} milliseconds
+ @returns {Promise<unknown>}
  */
 const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
 
 /**
- * Generates a key array for every letter of the given text with given number.
- * @param {string} text - The text to generate the key array for.
- * @param {number} number - The number to use for each item in the array.
+ Generates a key array for every letter of the given text with given number.
+ @param {string} text - The text to generate the key array for.
+ @param {number} number - The number to use for each item in the array.
  */
 export const generateKeyArray = (text, number) => {
 	const cleanedText = text.replaceAll(/\s/gv, '');
@@ -24,12 +24,12 @@ export const generateKeyArray = (text, number) => {
 };
 
 /**
- * Animates the given text to be revealed from either left to right or right to left.
- * @param {HTMLElement} element - The HTML element to animate.
- * @param {number} [steps=5] - The number of steps to count down each letter.
- * @param {number} [delay=0] - The delay between each count.
- * @param {boolean} [reverse=false] - Whether to count down from the end to the beginning.
- * @returns {Promise<void>}
+ Animates the given text to be revealed from either left to right or right to left.
+ @param {HTMLElement} element - The HTML element to animate.
+ @param {number} [steps=5] - The number of steps to count down each letter.
+ @param {number} [delay=0] - The delay between each count.
+ @param {boolean} [reverse=false] - Whether to count down from the end to the beginning.
+ @returns {Promise<void>}
  */
 export const animateTextCypher = async (element, steps = 5, delay = 0, reverse = false) => {
 	const text = element.textContent;
